071d3c0050598ebb797169da4f5f9db1124f40e2
jbenjaminy <young.bejamin@gmail.com>
2016-06-23 14:03:58 -0500

    Adds AJAX function

--- Apps/app.js
+++ Apps/app.js
@@ -1,22 +1,26 @@
-
+// http://api.eventful.com/rest/events/search?...&where=32.746682,-117.162741&within=25
+// // eventful key: NPmvmGSRSN5FwHF5
 
 $(function() {
 
 $('form').submit(function(event) {
     event.preventDefault();
-    var propertySearch = $('#zillow').val();
-    zillowSearch(propertySearch);
-        $('#zillow')[0].reset();
+    var eventSearch = $('#song-kick').val();
+    songKickSearch(eventSearch);
+        // $('#song-kick')[0].reset();
+        console.log(eventSearch, '<-- eventSearch');
 
 });
 
-    function zillowSearch(propertySearch) {
+    function songKickSearch(eventSearch) {
+        console.log(eventSearch);
         $.ajax({
-            method: "GET",
-            url: "http://api.recloud.me/1.0/sf/Property.search",
+            method: "GET http://eventful.com/oauth/authorize?oauth_token=NPmvmGSRSN5FwHF5",
+            url: "//api.eventful.com/json/events/search?q=music&location=" + eventSearch + "&date=Future&within=25",
             dataType: "json",
-            data:
-
+        })
+        .done(function(response){
+            console.log(response);
         });
     }
 
